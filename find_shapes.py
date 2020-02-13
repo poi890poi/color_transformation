5a0be20a181713c318979ca2ea132f709ed85359
Lim Lee <poi890poi@gmail.com>
2020-02-13 12:09:36 +0800

    Add rectangle area filtering and use LAB color.

--- find_shapes.py
+++ find_shapes.py
@@ -14,6 +14,7 @@ class ShapeDetector:
         shape = "unidentified"
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+        bounding = cv2.boundingRect(approx)
 
         # if the shape is a triangle, it will have 3 vertices
         if len(approx) == 3:
@@ -21,13 +22,9 @@ class ShapeDetector:
         # if the shape has 4 vertices, it is either a square or
         # a rectangle
         elif len(approx) == 4:
-            print('contour')
-            print(c)
-            print(approx)
-            print()
             # compute the bounding box of the contour and use the
             # bounding box to compute the aspect ratio
-            (x, y, w, h) = cv2.boundingRect(approx)
+            x, y, w, h = bounding
             ar = w / float(h)
             # a square will have an aspect ratio that is approximately
             # equal to one, otherwise, the shape is a rectangle
@@ -39,22 +36,25 @@ class ShapeDetector:
         else:
             shape = "circle"
         # return the name of the shape
-        return shape
+        return shape, approx, bounding
 
 
 # load the image
 image = cv2.imread(PATH_IN)
 resized = imutils.resize(image, width=WIDTH_OUT)
 src_width, src_height, src_channels = image.shape
-ratio = src_width / WIDTH_OUT
+dst_width, dst_height, *_ = resized.shape
+ratio = src_width / float(dst_width)
+print(ratio)
 cv2.imshow('Source', resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # convert the resized image to grayscale, blur it slightly,
 # and threshold it
-gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+lab = cv2.cvtColor(resized, cv2.COLOR_BGR2LAB)
+gray, a, b = cv2.split(lab)
+blurred = cv2.GaussianBlur(gray, (9, 9), 0)
 thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
             cv2.THRESH_BINARY, 11, 2)
 cv2.imshow('Binary', thresh)
@@ -79,18 +79,22 @@ for c in contours:
     # shape using only the contour
     M = cv2.moments(c)
     if M['m00'] == 0: continue
-    cX = int((M["m10"] / M["m00"]))
-    cY = int((M["m01"] / M["m00"]))
-    shape = sd.detect(c)
+    cX = int((M["m10"] / M["m00"]) * ratio)
+    cY = int((M["m01"] / M["m00"]) * ratio)
+    shape, poly, bounding = sd.detect(c)
     # multiply the contour (x, y)-coordinates by the resize ratio,
     # then draw the contours and the name of the shape on the image
     c = c.astype("float")
-    #c *= ratio
+    c *= ratio
     c = c.astype("int")
     if shape != 'square': continue
-    cv2.drawContours(resized, [c], -1, (0, 255, 0), 2)
-    cv2.putText(resized, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+    x, y, w, h = bounding
+    area = w * h / float(src_width) / float(src_height)
+    if area < 0.001: continue
+    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+    cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
         0.5, (255, 255, 255), 2)
     # show the output image
-    cv2.imshow("Image", resized)
-    cv2.waitKey(0)
+    cv2.imshow("Image", image)
+
+cv2.waitKey(0)
